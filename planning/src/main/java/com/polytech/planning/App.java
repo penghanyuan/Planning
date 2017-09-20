bb51cf5498b5991211c9243041894acba0f02a1c
Stéphane <boluge@gmail.com>
2017-09-20 15:05:54 +0200

    create project with maven

---
+++ planning/src/main/java/com/polytech/planning/App.java
@@ -0,0 +1,13 @@
+package com.polytech.planning;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
