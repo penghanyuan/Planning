1d708163a4da0ac4b088e654e33352e09bb4d7a9
Unknown <penghanyuan@163.com>
2017-09-26 19:39:05 +0200

    add getters and setters

--- planning/src/main/java/com/polytech/planning/model/Calendar.java
+++ planning/src/main/java/com/polytech/planning/model/Calendar.java
@@ -5,11 +5,26 @@ import java.util.List;
 
 public class Calendar {
 	private List<Semester> listSemester;
-	
+
 	/**
 	 * Calendar's Constructor
 	 */
 	public Calendar() {
 		listSemester = new ArrayList<Semester>();
 	}
+
+	/**
+	 * @return the listSemester
+	 */
+	public List<Semester> getListSemester() {
+		return listSemester;
+	}
+
+	/**
+	 * @param listSemester the listSemester to set
+	 */
+	public void setListSemester(List<Semester> listSemester) {
+		this.listSemester = listSemester;
+	}
+	
 }
