4ce31bb99283fc1180a77ef1fd8a1969eadbde28
Stéphane <boluge@gmail.com>
2017-10-09 18:02:39 +0200

    Auto stash before merge of "master" and "origin/master"

--- planning/src/main/java/com/polytech/planning/controller/ReadCalendar.java
+++ planning/src/main/java/com/polytech/planning/controller/ReadCalendar.java
@@ -3,79 +3,112 @@ package com.polytech.planning.controller;
 import com.polytech.planning.model.OriginalCalendar;
 
 public class ReadCalendar extends ReadFile {
-	
+
 	private OriginalCalendar calendar;
 
 	public ReadCalendar(String filePath) {
 		super(filePath);
 		calendar = new OriginalCalendar();
 	}
-	
+
 	public void readSemesters(int sheetNb) {
 		int blank = 0;
 		int columnNb = 2;
 		int i = 3;
-		
+
 		String[] buffer = new String[3];
-		
-		while(blank < 1) {
+
+		while (blank < 1) {
 			// if empty
 			try {
-				if(rowIsEmpty(i, sheetNb))
+				if (rowIsEmpty(i, sheetNb))
 					blank++;
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-						
-			for(int j = 0; j < 3; j++) {
+
+			for (int j = 0; j < 3; j++) {
 				try {
-					if(cellIsEmpty(i, j+columnNb,  sheetNb)) {
-						new Exception("La cellule ligne "+i+1+" et colonne "+j+columnNb+1+" est vide.");
+					if (cellIsEmpty(i, j + columnNb, sheetNb)) {
+						new Exception("La cellule ligne " + i + 1 + " et colonne " + j + columnNb + 1 + " est vide.");
 					} else {
-						buffer[j] = readCell(i, j+columnNb,  sheetNb);
+						buffer[j] = readCell(i, j + columnNb, sheetNb);
 					}
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
-			
+
 			// Store semester
-			calendar.addSemesters(buffer[0],buffer[1],buffer[2]);
-			
+			calendar.addSemesters(buffer[0], buffer[1], buffer[2]);
+
 			i++;
 		}
 	}
-	
+
 	public void readHolidays(int sheetNb) throws Exception {
 		int blank = 0;
 		int columnNb = 1;
 		int i = 3;
-		
+
 		String[] buffer = new String[3];
-		
-		while(blank < 1) {
-					
-			for(int j = 0; j < 3; j++) {
+
+		while (blank < 1) {
+
+			for (int j = 0; j < 3; j++) {
 				try {
-					if(cellIsEmpty(i, j+columnNb,  sheetNb)) {
-						new Exception("La cellule ligne "+i+1+" et colonne "+j+columnNb+1+" est vide.");
+					if (cellIsEmpty(i, j + columnNb, sheetNb)) {
+						new Exception("La cellule ligne " + i + 1 + " et colonne " + j + columnNb + 1 + " est vide.");
 					} else {
-						buffer[j] = readCell(i, j+columnNb,  sheetNb);
+						buffer[j] = readCell(i, j + columnNb, sheetNb);
 					}
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
-			
+
 			// Store Holiday
-			calendar.addHolidays(buffer[0],buffer[1],buffer[2]);
-			
+			calendar.addHolidays(buffer[0], buffer[1], buffer[2]);
+
 			i++;
 		}
 	}
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}	
+	public void readFreeDays(int sheetNb) {
+		int blank = 0;
+		int columnNb = 1;
+		int i = 3;
+
+		String[] buffer = new String[3];
+		while (blank < 1) {
+			for (int j = 0; j < 3; j++) {
+				try {
+					if (cellIsEmpty(i, j + columnNb, sheetNb)) {
+						new Exception("La cellule ligne " + i + 1 + " et colonne " + j + columnNb + 1 + " est vide.");
+					} else {
+						buffer[j] = readCell(i, j + columnNb, sheetNb);
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+
+			// Store free days
+			try {
+				calendar.addFreeDays(buffer[0], buffer[1], buffer[2]);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			i++;
+		}
+	}
+
+	/**
+	 * @return the calendar
+	 */
+	public OriginalCalendar getCalendar() {
+		return calendar;
+	}
+
 }
--- planning/src/main/java/com/polytech/planning/controller/ReadMockUp.java
+++ planning/src/main/java/com/polytech/planning/controller/ReadMockUp.java
@@ -1,17 +1,10 @@
 package com.polytech.planning.controller;
 
-import java.util.HashMap;
-import java.util.List;
-
-import com.polytech.planning.model.OriginalCourse;
-
 public class ReadMockUp extends ReadFile {
 
 	public ReadMockUp(String filePath) {
 		super(filePath);
 	}
 	
-	public HashMap<String,List<OriginalCourse>> readData(){
-		return null;
-	}
+	
 }
