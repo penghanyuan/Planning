b32bcd340b2b2ec740c7079fa13f05142fba4271
Stéphane <boluge@gmail.com>
2017-10-30 10:48:34 +0100

    add styles and custom

--- planning/src/main/java/com/polytech/planning/controller/StylesLib.java
+++ planning/src/main/java/com/polytech/planning/controller/StylesLib.java
@@ -2,10 +2,29 @@ package com.polytech.planning.controller;
 
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.*;
+import org.junit.experimental.theories.Theories;
 
 public final class StylesLib {
 
-	public static CellStyle baseStyle(XSSFWorkbook wb) {
+	private static XSSFColor cmBg;
+	private static XSSFColor tdBg;
+	private static XSSFColor tpBg;
+	private static XSSFColor ccEtBg;
+	private static XSSFColor dateBg;
+	private static XSSFColor weekNumBg;
+	private static XSSFColor holidaysBg;
+
+	public static XSSFCellStyle baseStyle(XSSFWorkbook wb) {
+		// Define color palette
+		cmBg = new XSSFColor(new java.awt.Color(33, 150, 243, 0)); // #2196F3
+		tdBg = new XSSFColor(new java.awt.Color(139, 195, 74, 0)); // #8BC34A
+		tpBg = new XSSFColor(new java.awt.Color(255, 193, 7, 0)); // #FFC107
+		ccEtBg = new XSSFColor(new java.awt.Color(244, 67, 54, 0)); // #F44336
+
+		dateBg = new XSSFColor(new java.awt.Color(189, 189, 189, 0)); // #BDBDBD
+		weekNumBg = new XSSFColor(new java.awt.Color(224, 224, 224, 0)); // #E0E0E0
+		holidaysBg = new XSSFColor(new java.awt.Color(255, 87, 34, 0)); // #FF5722
+
 		// create font
 		Font font = wb.createFont();
 		font.setFontHeightInPoints((short) 12);
@@ -13,41 +32,86 @@ public final class StylesLib {
 		font.setColor(IndexedColors.BLACK.getIndex());
 		font.setBold(false);
 		font.setItalic(false);
-		
+
 		// Create cell style
-		CellStyle cellStyle = wb.createCellStyle();
-        cellStyle.setAlignment(HorizontalAlignment.LEFT);
-        cellStyle.setVerticalAlignment(VerticalAlignment.CENTER);	
-		
+		XSSFCellStyle cellStyle = wb.createCellStyle();
+		cellStyle.setAlignment(HorizontalAlignment.CENTER);
+		cellStyle.setVerticalAlignment(VerticalAlignment.CENTER);
+
 		// Setting font to style
 		cellStyle.setFont(font);
 
 		return cellStyle;
 	}
-	
-	public static CellStyle cmStyle(XSSFWorkbook wb) {
-		CellStyle cellStyle = baseStyle(wb);
+
+	public static XSSFCellStyle cmStyle(XSSFWorkbook wb) {
+		XSSFCellStyle cellStyle = baseStyle(wb);
 		cellStyle.setAlignment(HorizontalAlignment.CENTER);
-		cellStyle.setFillForegroundColor(IndexedColors.TURQUOISE.getIndex());
+		cellStyle.setFillForegroundColor(cmBg);
 		cellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
 
 		return cellStyle;
 	}
-	
-	public static CellStyle tdStyle(XSSFWorkbook wb) {
-		CellStyle cellStyle = baseStyle(wb);
+
+	public static XSSFCellStyle tdStyle(XSSFWorkbook wb) {
+		XSSFCellStyle cellStyle = baseStyle(wb);
 		cellStyle.setAlignment(HorizontalAlignment.CENTER);
-		cellStyle.setFillForegroundColor(IndexedColors.BRIGHT_GREEN1.getIndex());
+		cellStyle.setFillForegroundColor(tdBg);
 		cellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
 
 		return cellStyle;
 	}
-	
-	public static CellStyle tpStyle(XSSFWorkbook wb) {
-		CellStyle cellStyle = baseStyle(wb);
+
+	public static XSSFCellStyle tpStyle(XSSFWorkbook wb) {
+		XSSFCellStyle cellStyle = baseStyle(wb);
 		cellStyle.setAlignment(HorizontalAlignment.CENTER);
-		cellStyle.setFillForegroundColor(IndexedColors.YELLOW1.getIndex());
+		cellStyle.setFillForegroundColor(tpBg);
 		cellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
+		return cellStyle;
+	}
+
+	public static XSSFCellStyle ccStyle(XSSFWorkbook wb) {
+		XSSFCellStyle cellStyle = baseStyle(wb);
+		cellStyle.setAlignment(HorizontalAlignment.CENTER);
+		cellStyle.setFillForegroundColor(ccEtBg);
+		cellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
+		return cellStyle;
+	}
+
+	public static XSSFCellStyle weekNumStyle(XSSFWorkbook wb) {
+		XSSFCellStyle cellStyle = baseStyle(wb);
+		cellStyle.setAlignment(HorizontalAlignment.CENTER);
+		cellStyle.setFillForegroundColor(weekNumBg);
+		cellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
+		return cellStyle;
+	}
+
+	public static XSSFCellStyle dateStyle(XSSFWorkbook wb) {
+		XSSFCellStyle cellStyle = baseStyle(wb);
+		cellStyle.setAlignment(HorizontalAlignment.CENTER);
+		cellStyle.setFillForegroundColor(dateBg);
+		cellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
+		return cellStyle;
+	}
+
+	public static XSSFCellStyle holidayStyle(XSSFWorkbook wb) {
+		XSSFCellStyle cellStyle = baseStyle(wb);
+		cellStyle.setAlignment(HorizontalAlignment.CENTER);
+		cellStyle.setFillForegroundColor(holidaysBg);
+		cellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
+		return cellStyle;
+	}
+
+	public static XSSFCellStyle setBold(XSSFCellStyle cellStyle) {
+		Font font = cellStyle.getFont();
+		font.setBold(true);
+		
+		cellStyle.setFont(font);
 
 		return cellStyle;
 	}
--- planning/src/test/java/com/polytech/planning/StyleLibTest.java
+++ planning/src/test/java/com/polytech/planning/StyleLibTest.java
@@ -26,21 +26,37 @@ public class StyleLibTest {
 		Sheet sheet1 = workbook.createSheet("Test de styles");
 		Row row = sheet1.createRow(0);
 		Cell cell = row.createCell(0);
+		Cell cellB = row.createCell(2);
+		
 		Row rowCM = sheet1.createRow(1);
 		Cell cellCM = rowCM.createCell(0);
+		
 		Row rowTD = sheet1.createRow(2);
 		Cell cellTD = rowTD.createCell(0);
+		
 		Row rowTP = sheet1.createRow(3);
 		Cell cellTP = rowTP.createCell(0);
+		
+		Row rowCC = sheet1.createRow(4);
+		Cell cellCC = rowCC.createCell(0);
+		
+		Cell cellWeekNum = rowTD.createCell(2);
+		Cell cellDate = rowCM.createCell(2);
+		Cell cellHoliday = rowCC.createCell(2);
 
 		sheet1.setColumnWidth(0, 24 * 200);
+		sheet1.setColumnWidth(2, 24 * 200);
 		row.setHeightInPoints(24);
 		rowCM.setHeightInPoints(24);
 		rowTD.setHeightInPoints(24);
 		rowTP.setHeightInPoints(24);
+		rowCC.setHeightInPoints(24);
 
 		cell.setCellValue("Style de base");
 		cell.setCellStyle(StylesLib.baseStyle(workbook));
+		
+		cellB.setCellValue("Style bold");
+		cellB.setCellStyle(StylesLib.setBold(StylesLib.baseStyle(workbook)));
 		
 		cellCM.setCellValue("Style CM");
 		cellCM.setCellStyle(StylesLib.cmStyle(workbook));
@@ -50,6 +66,18 @@ public class StyleLibTest {
 		
 		cellTP.setCellValue("Style TP");
 		cellTP.setCellStyle(StylesLib.tpStyle(workbook));
+		
+		cellCC.setCellValue("Style CC");
+		cellCC.setCellStyle(StylesLib.ccStyle(workbook));
+		
+		cellWeekNum.setCellValue("Style Week Num");
+		cellWeekNum.setCellStyle(StylesLib.weekNumStyle(workbook));
+		
+		cellDate.setCellValue("Style Date");
+		cellDate.setCellStyle(StylesLib.dateStyle(workbook));
+		
+		cellHoliday.setCellValue("Style Holiday");
+		cellHoliday.setCellStyle(StylesLib.holidayStyle(workbook));
 
 		FileOutputStream output;
 		try {
