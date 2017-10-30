939af66ccbe86fcc50da7347a38c41e28fc2a945
Stéphane <boluge@gmail.com>
2017-10-30 16:10:48 +0100

    add WriteFile et change WritePlanning

--- planning/src/main/java/com/polytech/planning/controller/WriteFile.java
+++ planning/src/main/java/com/polytech/planning/controller/WriteFile.java
@@ -4,34 +4,39 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import com.polytech.planning.model.Planning;
-
 public class WriteFile {
 
 	private Workbook workbook;
 	private String filePath;
 
 	/**
+	 * Constructor
+	 * 
 	 * @param filePath
 	 */
 	public WriteFile(String filePath) {
 		this.filePath = filePath;
-	}
 
-	/**
-	 * Write Planning in to excel file.
-	 * 
-	 * @param planning
-	 */
-	public void WritePlanning(Planning planning) {
 		// si file finit par xls ou xlsx
 		if (filePath.substring(filePath.length() - 4).equals(".xls")) {
 			workbook = new HSSFWorkbook();
 		} else {
 			workbook = new XSSFWorkbook();
 		}
+	}
 
-		// ecriture dans le fichier (creation des sheets)
+	/**
+	 * @return the workbook
+	 */
+	public Workbook getWorkbook() {
+		return workbook;
+	}
+
+	/**
+	 * @return the filePath
+	 */
+	public String getFilePath() {
+		return filePath;
 	}
 
 }
---
+++ planning/src/main/java/com/polytech/planning/controller/WritePlanning.java
@@ -0,0 +1,23 @@
+package com.polytech.planning.controller;
+
+import com.polytech.planning.model.Planning;
+
+public class WritePlanning extends WriteFile {
+
+	/**
+	 * @param filePath
+	 */
+	public WritePlanning(String filePath) {
+		super(filePath);
+	}
+
+	/**
+	 * Write Planning in to excel file.
+	 * 
+	 * @param planning
+	 */
+	public void WritePlanningFile(Planning planning) {
+		
+	}
+
+}
