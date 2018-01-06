3abae596207e04b046704e321e954feac95a6827
Unknown <penghanyuan@163.com>
2018-01-06 19:02:12 +0100

    add help option

--- planning/src/main/java/com/ploytech/planning/view/MainView.java
+++ planning/src/main/java/com/ploytech/planning/view/MainView.java
@@ -20,14 +20,19 @@ public class MainView {
 	 * @throws Exception
 	 */
 	public void parseCommand() throws Exception {
-		String file_name = "Planning";
+		String file_name;
 		String year;
 		String maquettePath;
 		String calendarPath;
 		String schoolYear;
 		String[] schoolYearTable;
 		if (command.length < 4) {
-			throw new Exception("Command error! Please check");
+			if (command.length == 1 && command[0].equals("-h")) {
+				this.help();
+			} else {
+				throw new Exception(
+						"Command error! Please check. You can enter 'java -jar planning-generator.jar -h' to get help.");
+			}
 		} else {
 			schoolYear = command[0];
 			year = command[1];
@@ -35,20 +40,20 @@ public class MainView {
 			calendarPath = command[3];
 			schoolYearTable = schoolYear.split("/");
 			if (year.equals("-di3")) {
-				file_name += " Année " + "3 DI " + schoolYearTable[0] + " - " + schoolYearTable[1] + ".xlsx";
+				file_name = "Planning Année " + "3 DI " + schoolYearTable[0] + " - " + schoolYearTable[1] + ".xlsx";
 				this.excuteCommand(schoolYear, "DI3", maquettePath, calendarPath, file_name);
 			} else if (year.equals("-di4")) {
-				file_name += " Année " + "4 DI " + schoolYearTable[0] + " - " + schoolYearTable[1] + ".xlsx";
+				file_name = "Planning Année " + "4 DI " + schoolYearTable[0] + " - " + schoolYearTable[1] + ".xlsx";
 				this.excuteCommand(schoolYear, "DI4", maquettePath, calendarPath, file_name);
 			} else if (year.equals("-di5")) {
-				file_name += " Année " + "5 DI " + schoolYearTable[0] + " - " + schoolYearTable[1] + ".xlsx";
+				file_name = "Planning Année " + "5 DI " + schoolYearTable[0] + " - " + schoolYearTable[1] + ".xlsx";
 				this.excuteCommand(schoolYear, "DI5", maquettePath, calendarPath, file_name);
 			} else if (year.equals("-all")) {
-				file_name += " Année " + "3 DI " + schoolYearTable[0] + " - " + schoolYearTable[1] + ".xlsx";
+				file_name = "Planning Année " + "3 DI " + schoolYearTable[0] + " - " + schoolYearTable[1] + ".xlsx";
 				this.excuteCommand(schoolYear, "DI3", maquettePath, calendarPath, file_name);
-				file_name += " Année " + "4 DI " + schoolYearTable[0] + " - " + schoolYearTable[1] + ".xlsx";
+				file_name = "Planning Année " + "4 DI " + schoolYearTable[0] + " - " + schoolYearTable[1] + ".xlsx";
 				this.excuteCommand(schoolYear, "DI4", maquettePath, calendarPath, file_name);
-				file_name += " Année " + "5 DI " + schoolYearTable[0] + " - " + schoolYearTable[1] + ".xlsx";
+				file_name = "Planning Année " + "5 DI " + schoolYearTable[0] + " - " + schoolYearTable[1] + ".xlsx";
 				this.excuteCommand(schoolYear, "DI5", maquettePath, calendarPath, file_name);
 			} else {
 				throw new Exception("Command error! Please check");
@@ -65,10 +70,39 @@ public class MainView {
 			gp = new GeneratePlanning(school_year, maquette_path, calendar_path);
 			wp = new WritePlanning(gp.getPlanningByYear(annee), annee, file_name);
 			wp.createFile();
-			System.out.println("Finished!");
+			System.out.println("Creating <" + file_name + "> finished");
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
+	}
+
+	private void help() {
+		System.out.println("------------------------------------------------------------");
+		System.out.println("The format of command is:");
+		System.out.println(
+				"java -jar planning-generator.jar <school_year> <-di3|-di4|-di5|-all> <path_of_maquette> <path_of_calendar>");
+		System.out.println("------------------------------------------------------------");
+		System.out.println("Explication:");
+		System.out.println("\t <school_year> : format-> yearStart/yearEnd");
+		System.out.println("\t\t Obligatory, the school year of the planning. eg. 2017/2018");
+		System.out.println("\t");
+		System.out.println("\t <-di3|-di4|-di5|-all> :");
+		System.out.println("\t\t Obligatory, the year of the planning you want to generate.");
+		System.out.println("\t\t\t -di3: for DI3");
+		System.out.println("\t\t\t -di4: for DI4");
+		System.out.println("\t\t\t -di5: for DI5");
+		System.out.println("\t\t\t -all: for all three years");
+		System.out.println("\t");
+		System.out.println("\t <path_of_maquette> :");
+		System.out.println("\t\t Obligatory, the file path of the maquette");
+		System.out.println("\t");
+		System.out.println("\t <path_of_calendar> :");
+		System.out.println("\t\t Obligatory, the file path of the calendar");
+		System.out.println("\t");
+		System.out.println("Note: the planning will be genereted at the same place with planning-generator.jar\n"
+				+ "The name of the file will be 'Planning Année <year> DI <school_year_start> - <school_year_end>.xlsx'");
+		System.out.println("\t");
+		System.out.println("\t");
 	}
 
 }
